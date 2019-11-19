0558057a8f753a8ccd6a31fcd618f74c4acb538e
HyunGyu-Lee <gusrb0808@naver.com>
2019-11-19 18:03:09 +0900

    Summary 기능 추가

--- src/components/todoapp/TodoSummary.vue
+++ src/components/todoapp/TodoSummary.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-row dense align="center" class="pl-0 pr-0 ml-0 mr-0">
+  <v-row dense align="center" class="pl-0 pr-0 ml-0 mr-0" v-if="todoSummary">
     <v-col class="text-right">
       <div class="status mr-3">
         <i class="todo-status-todo-stick" style="background"></i>
         <span class="caption mr-1">할 일</span>
-        <span class="caption">0</span>
+        <span class="caption">{{todoCount}}</span>
       </div>
       <div class="status mr-2">
         <i class="todo-status-in-progress-stick"></i>
         <span class="caption mr-1">진행 중</span>
-        <span class="caption">0</span>        
+        <span class="caption">{{inprogressCount}}</span>        
       </div>
       <div class="status">
         <i class="todo-status-done-stick"></i>
         <span class="caption mr-1">완료</span>
-        <span class="caption">0</span>
+        <span class="caption">{{doneCount}}</span>
       </div>
     </v-col>
   </v-row>
@@ -22,6 +22,23 @@
 <script>
 // import TodoBiz from '@/modules/biz/todo'
 export default {
+  props: {
+    todoSummary: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    todoCount() {
+      return this.todoSummary.statusCounts[0]
+    },
+    inprogressCount() {
+      return this.todoSummary.statusCounts[1]
+    },
+    doneCount() {
+      return this.todoSummary.statusCounts[2]
+    }
+  }
 }
 </script>
 
