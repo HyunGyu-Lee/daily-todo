9ad105d8cfa552a68c228e3cf5dec7f5cfaa66a0
HyunGyu-Lee <gusrb0808@naver.com>
2019-12-04 22:09:57 +0900

    데이터 갱신

--- src/pages/HealthPage.vue
+++ src/pages/HealthPage.vue
@@ -3,10 +3,12 @@
     <h1>Health!</h1>
     <br><br><br>
     <ul>
-      <li>2019-12-03 start 84.3</li>
-      <li>2019-12-03 end 84.5</li> 
+      <li>2019-12-02 start 84.3</li>
+      <li>2019-12-02 end 84.5</li> 
+      <li>2019-12-03 start 83.8</li>
+      <li>2019-12-03 end 84.3</li>       
       <li>2019-12-04 start 83.8</li>
-      <li>2019-12-04 end 84.3</li>       
+      <li>2019-12-04 end ?</li>
     </ul>
   </v-row>
 </template>
